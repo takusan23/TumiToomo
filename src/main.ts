254f13a531e58dee968db04e952f0e7fe9d675e6
takusan23 <takumu1200@gmail.com>
2020-02-09 01:55:43 +0900

    タイトル追加＋物体生成をクリックを離した２秒後に動くように

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as b2 from "@akashic-extension/akashic-box2d"
 declare const window: RPGAtsumaruWindow
 
 export function main(param: GameMainParameterObject): void {
+
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
@@ -27,252 +28,275 @@ export function main(param: GameMainParameterObject): void {
 	// 生成した物体の配列
 	const bodyObjectList: b2.Box2DOptions.EBody[] = []
 
-	scene.loaded.add(() => {
-		// ここからゲーム内容を記述します
+	/** タイトル画面作成 */
+	const titleScene = new g.Scene({ game: g.game, assetIds: ["title"] })
+	// 読み込みが終わったら
+	titleScene.loaded.add(() => {
+		// 画像追加
+		const titleSprite = new g.Sprite({ scene: titleScene, src: titleScene.assets["title"] })
+		titleScene.append(titleSprite)
+		// 5秒後にスタート
+		titleScene.setTimeout(() => {
+			// 5秒引いておく（タイトル表示時間）
+			time -= 5
+			scene.loaded.add(() => {
+				// ここからゲーム内容を記述します
 
-		// 物理エンジンの世界を生成する
-		const worldOption = {
-			gravity: [0, 9.8],
-			scale: 50,
-			sleep: true
-		}
-		const box = new b2.Box2D(worldOption)
-		// 物理エンジンの世界を進める
-		scene.update.add(() => {
-			box.step(1 / g.game.fps)
-		})
-
-		// フォントの生成
-		const font = new g.DynamicFont({
-			game: g.game,
-			fontFamily: g.FontFamily.Serif,
-			size: 48
-		})
-
-		// スコア表示用のラベル
-		const scoreLabel = new g.Label({
-			scene: scene,
-			text: "SCORE: 0",
-			font: font,
-			fontSize: font.size / 2,
-			textColor: "black"
-		})
-		scene.append(scoreLabel)
-
-		// 残り時間表示用ラベル
-		const timeLabel = new g.Label({
-			scene: scene,
-			text: "TIME: 0",
-			font: font,
-			fontSize: font.size / 2,
-			textColor: "black",
-			x: 0.7 * g.game.width
-		})
-		scene.append(timeLabel)
-
-		// 地面
-		const base = new g.FilledRect({
-			scene: scene,
-			width: g.game.width,
-			height: 10,
-			cssColor: "black",
-			y: (300)
-		})
-		scene.append(base)
-		const floorFixDef = box.createFixtureDef({
-			density: 1.0, // 密度
-			friction: 0.5, // 摩擦係数
-			restitution: 0.3, // 反発係数
-			shape: box.createRectShape(base.width, base.height) // 形状
-		})
-		const floorDef = box.createBodyDef({
-			type: b2.BodyType.Static
-		})
-		const floorBody = box.createBody(base, floorDef, floorFixDef)
-
-		// 物体生成
-		const nKou: BodyObject = {
-			assetSrc: "n_kou",
-			sharpList: [
-				box.vec2(25, -25),
-				box.vec2(25, 25),
-				box.vec2(-25, 25),
-				box.vec2(-25, -25)
-			]
-		}
-		const bodyList = [nKou]
-
-		/** 触っていないオブジェクトが存在する場合はtrue
-		 * （まだ上にあって落下していないときはtrue）
-		 */
-		let isNotTouchObjctExists = false
-
-		/** 物体の生成関数。
-		 * @param obj 当たり判定とか画像の名前とか
-		 * @param xPos X座標。クリックしたときに取れるのでそれ
-		 * @param yPos Y座標。クリックしたときに取れるのでそれ
-		 */
-		const createObject = (obj: BodyObject, xPos: number): b2.Box2DOptions.EBody => {
-			isNotTouchObjctExists = true
-			const entity = new g.Sprite({
-				scene: scene,
-				src: scene.assets[obj.assetSrc],
-				width: (scene.assets[obj.assetSrc] as g.ImageAsset).width,
-				height: (scene.assets[obj.assetSrc] as g.ImageAsset).height,
-				x: xPos,
-				y: getHighestPos(base.y) - 100
-			})
-			scene.append(entity)
-			entity.modified()
-			const entityFixDef = box.createFixtureDef({
-				density: 1.0, // 密度
-				friction: 0.5, // 摩擦係数
-				restitution: 0.3, // 反発係数
-				shape: box.createPolygonShape(obj.sharpList) // 形状
-			})
-			const entityDef = box.createBodyDef({
-				type: b2.BodyType.Dynamic
-			})
-			const createBody = box.createBody(entity, entityDef, entityFixDef)
-
-			// Q:これはなにか？ A:もし動かしてるときに詰んでたたわーが崩れるとカメラが移動する→移動中の物体が見切れる　これを対策するため
-			entity.update.add(() => {
-				entity.y = getHighestPos(base.y) - 100
-				// Sprite移動だけではだめなので
-				createBody.b2body.SetPosition(box.vec2(createBody.entity.x + entity.width / 2, createBody.entity.y + entity.height / 2))
-				entity.modified()
-			})
-
-			// 押すまでオブジェクトを睡眠？停止状態にする。寝てる間はDynamicでも動かない
-			// 睡眠状態を利用可能にする
-			createBody.b2body.SetSleepingAllowed(true)
-			// おやすみ
-			createBody.b2body.SetAwake(false)
-			// X座標取得。ドラッグでできるように
-			scene.pointMoveCapture.add((event) => {
-				createBody.entity.x += event.prevDelta.x
-				// Sprite移動だけではだめなので
-				createBody.b2body.SetPosition(box.vec2(createBody.entity.x + entity.width / 2, createBody.entity.y + entity.height / 2))
-				createBody.entity.modified()
-			})
-			scene.pointUpCapture.add(() => {
-				// おはようのオーディションして～かみｇ
-				createBody.b2body.SetAwake(true)
-				isNotTouchObjctExists = false
-				// 落としたら移動できないようにイベント消す
-				scene.pointMoveCapture.removeAll()
-				scene.pointUpCapture.removeAll()
-				entity.update.removeAll()
-				bodyObjectList.push(createBody)
-			})
-			return createBody
-		}
-		// 画像生成。
-		scene.setInterval(() => {
-			if (!isNotTouchObjctExists) {
-				const bodyObj = createObject(nKou, (g.game.width / 2))
-			}
-		}, 2 * 1000)
-
-		// カメラ移動、スコアラベル、時間ラベル移動など
-		const camera = new g.Camera2D({ game: g.game })
-		g.game.focusingCamera = camera
-		g.game.modified = true
-		scene.update.add(() => {
-			// 一番高いところにある物体のの座標
-			const highest = getHighestPos(0)
-			// 一番低いところにある物体のの座標。今回は受け皿
-			const lowest = base.y
-			console.log(`一番高い:${Math.round(highest)} / 一番低い:${Math.round(lowest)}`)
-			// 絶対値計算
-			let abs = Math.abs(lowest - highest)
-			if (bodyObjectList.length === 0) {
-				abs = 0
-			}
-			console.log(abs > g.game.height)
-			if (abs > g.game.height - 100) {
-				camera.y = highest - 100
-				camera.modified()
-			} else {
-				camera.y = 0
-				camera.modified()
-			}
-			// スコア、残り時間も移動
-			scoreLabel.y = camera.y
-			scoreLabel.modified()
-			timeLabel.y = camera.y
-			timeLabel.modified()
-			// 一番高いところをスコアに
-			// 小数点以下切り捨て
-			g.game.vars.gameState.score = Math.round(abs)
-			scoreLabel.text = `SCORE: ${g.game.vars.gameState.score}`
-			scoreLabel.invalidate()
-		})
-
-		/** 一番高いところにある物体の高さを取得します。
-		 * @param defaultValue もし取れなかった場合は返り値になります
-		 * @returns 一番高いY座標。
-		 */
-		const getHighestPos = (defaultValue: number = 0): number => {
-			let pos = defaultValue
-			const tmpList = bodyObjectList.concat()
-			if (tmpList.length > 0) {
-				tmpList.sort((a, b) => {
-					if (a.entity.y < b.entity.y) return -1
-					if (a.entity.y > b.entity.y) return 1
-					return 0
-				})
-				pos = tmpList[0].entity.y
-			}
-			return pos
-		}
-
-		/** 一番低いところにある物体の底辺の座標を取得します。
-		 * 例えば■が一番下にあったとき、■のY座標から■の高さだけ引いたものです。
-		 * @param defaultValue もし取れなかった場合は返り値になります
-		 * @returns 低いところにある物体の底辺の座標。
-		 */
-		const getLowestPos = (defaultValue: number = 0): number => {
-			let pos = defaultValue
-			const tmpList: b2.Box2DOptions.EBody[] = []
-			// 画面外の物体はいらない
-			bodyObjectList.forEach(obj => {
-				if (obj.entity.y <= g.game.height) {
-					tmpList.push(obj)
+				// 物理エンジンの世界を生成する
+				const worldOption = {
+					gravity: [0, 9.8],
+					scale: 50,
+					sleep: true
 				}
-			})
-			if (tmpList.length > 0) {
-				tmpList.sort((a, b) => {
-					if (a.entity.y < b.entity.y) return 1
-					if (a.entity.y > b.entity.y) return -1
-					return 0
+				const box = new b2.Box2D(worldOption)
+				// 物理エンジンの世界を進める
+				scene.update.add(() => {
+					box.step(1 / g.game.fps)
 				})
-				pos = tmpList[0].entity.y - tmpList[0].entity.height
-			}
-			return pos
-		}
 
-		const updateHandler = () => {
-			if (time <= 0) {
-				// RPGアツマール環境であればランキングを表示します
-				if (param.isAtsumaru) {
-					const boardId = 1
-					window.RPGAtsumaru.experimental.scoreboards.setRecord(boardId, g.game.vars.gameState.score).then(function () {
-						window.RPGAtsumaru.experimental.scoreboards.display(boardId)
+				// フォントの生成
+				const font = new g.DynamicFont({
+					game: g.game,
+					fontFamily: g.FontFamily.SansSerif,
+					size: 48
+				})
+
+				// スコア表示用のラベル
+				const scoreLabel = new g.Label({
+					scene: scene,
+					text: "スコア: 0",
+					font: font,
+					fontSize: font.size / 2,
+					textColor: "black"
+				})
+				scene.append(scoreLabel)
+
+				// 残り時間表示用ラベル
+				const timeLabel = new g.Label({
+					scene: scene,
+					text: "残り時間: 60",
+					font: font,
+					fontSize: font.size / 2,
+					textColor: "black",
+					x: 0.7 * g.game.width
+				})
+				scene.append(timeLabel)
+
+				// 地面
+				const base = new g.FilledRect({
+					scene: scene,
+					width: g.game.width,
+					height: 10,
+					cssColor: "black",
+					y: (300)
+				})
+				scene.append(base)
+				const floorFixDef = box.createFixtureDef({
+					density: 1.0, // 密度
+					friction: 0.5, // 摩擦係数
+					restitution: 0.3, // 反発係数
+					shape: box.createRectShape(base.width, base.height) // 形状
+				})
+				const floorDef = box.createBodyDef({
+					type: b2.BodyType.Static
+				})
+				const floorBody = box.createBody(base, floorDef, floorFixDef)
+
+				// 物体生成
+				const nKou: BodyObject = {
+					assetSrc: "n_kou",
+					sharpList: [
+						box.vec2(25, -25),
+						box.vec2(25, 25),
+						box.vec2(-25, 25),
+						box.vec2(-25, -25)
+					]
+				}
+				const bodyList = [nKou]
+
+				/** 一番高いところにある物体の高さを取得します。
+				 * @param defaultValue もし取れなかった場合は返り値になります
+				 * @returns 一番高いY座標。
+				 */
+				const getHighestPos = (defaultValue: number = 0): number => {
+					let pos = defaultValue
+					const tmpList = bodyObjectList.concat()
+					if (tmpList.length > 0) {
+						tmpList.sort((a, b) => {
+							if (a.entity.y < b.entity.y) return -1
+							if (a.entity.y > b.entity.y) return 1
+							return 0
+						})
+						pos = tmpList[0].entity.y
+					}
+					return pos
+				}
+				/** 触っていないオブジェクトが存在する場合はtrue
+				 * （まだ上にあって落下していないときはtrue）
+				 */
+				let isNotTouchObjctExists = false
+				/** 物体の生成関数。
+				 * @param obj 当たり判定とか画像の名前とか
+				 * @param xPos X座標。クリックしたときに取れるのでそれ
+				 */
+				const createObject = (obj: BodyObject, xPos: number): b2.Box2DOptions.EBody => {
+					isNotTouchObjctExists = true
+					const entity = new g.Sprite({
+						scene: scene,
+						src: scene.assets[obj.assetSrc],
+						width: (scene.assets[obj.assetSrc] as g.ImageAsset).width,
+						height: (scene.assets[obj.assetSrc] as g.ImageAsset).height,
+						x: xPos,
+						y: getHighestPos(base.y) - 100
 					})
+					scene.append(entity)
+					entity.modified()
+					const entityFixDef = box.createFixtureDef({
+						density: 1.0, // 密度
+						friction: 0.5, // 摩擦係数
+						restitution: 0.3, // 反発係数
+						shape: box.createPolygonShape(obj.sharpList) // 形状
+					})
+					const entityDef = box.createBodyDef({
+						type: b2.BodyType.Dynamic
+					})
+					const createBody = box.createBody(entity, entityDef, entityFixDef)
+
+					// Q:これはなにか？ A:もし動かしてるときに詰んでたたわーが崩れるとカメラが移動する→移動中の物体が見切れる　これを対策するため
+					entity.update.add(() => {
+						entity.y = getHighestPos(base.y) - 100
+						// Sprite移動だけではだめなので
+						createBody.b2body.SetPosition(box.vec2(createBody.entity.x + entity.width / 2, createBody.entity.y + entity.height / 2))
+						entity.modified()
+					})
+
+					// 押すまでオブジェクトを睡眠？停止状態にする。寝てる間はDynamicでも動かない
+					// 睡眠状態を利用可能にする
+					createBody.b2body.SetSleepingAllowed(true)
+					// おやすみ
+					createBody.b2body.SetAwake(false)
+					// X座標取得。ドラッグでできるように
+					scene.pointMoveCapture.add((event) => {
+						createBody.entity.x += event.prevDelta.x
+						// Sprite移動だけではだめなので
+						createBody.b2body.SetPosition(box.vec2(createBody.entity.x + entity.width / 2, createBody.entity.y + entity.height / 2))
+						createBody.entity.modified()
+					})
+					// クリックを離したとき。
+					const pointDown = () => {
+						// おはようのオーディションして～かみｇ
+						createBody.b2body.SetAwake(true)
+						isNotTouchObjctExists = false
+						// 落としたら移動できないようにイベント消す
+						scene.pointMoveCapture.removeAll()
+						// クリックを離したときは他でも使っているためremoveAllすると影響受けるので
+						scene.pointUpCapture.remove(pointDown)
+						// カメラの移動と合わせて動かすイベント削除
+						entity.update.removeAll()
+						// 配列追加（一番高いなどを求めるときに使う。）
+						bodyObjectList.push(createBody)
+					}
+					// クリックを離したときイベント登録。
+					scene.pointUpCapture.add(pointDown)
+					return createBody
 				}
-				scene.update.remove(updateHandler) // カウントダウンを止めるためにこのイベントハンドラを削除します
-			}
-			// カウントダウン処理
-			time -= 1 / g.game.fps
-			timeLabel.text = "TIME: " + Math.ceil(time)
-			timeLabel.invalidate()
-		}
-		scene.update.add(updateHandler)
+				// 画像生成。
+				createObject(nKou, (g.game.width / 2))
+				scene.pointUpCapture.add(() => {
+					setTimeout(() => { createObject(nKou, (g.game.width / 2)) }, 2000)
+				})
 
-		// ここまでゲーム内容を記述します
+				// カメラ移動、スコアラベル、時間ラベル移動など
+				const camera = new g.Camera2D({ game: g.game })
+				g.game.focusingCamera = camera
+				g.game.modified = true
+				scene.update.add(() => {
+					// 一番高いところにある物体のの座標
+					const highest = getHighestPos(0)
+					// 一番低いところにある物体のの座標。今回は受け皿
+					const lowest = base.y
+					// 絶対値計算
+					let abs = Math.abs(lowest - highest)
+					if (bodyObjectList.length === 0) {
+						abs = 0
+					}
+					if (abs > g.game.height - 100) {
+						camera.y = highest - 100
+						camera.modified()
+					} else {
+						camera.y = 0
+						camera.modified()
+					}
+					// スコア、残り時間も移動
+					scoreLabel.y = camera.y
+					scoreLabel.modified()
+					timeLabel.y = camera.y
+					timeLabel.modified()
+					// 一番高いところをスコアに
+					// 小数点以下切り捨て
+					g.game.vars.gameState.score = Math.round(abs)
+					scoreLabel.text = `スコア: ${g.game.vars.gameState.score}`
+					scoreLabel.invalidate()
+				})
+				/** 一番低いところにある物体の底辺の座標を取得します。
+				 * 例えば■が一番下にあったとき、■のY座標から■の高さだけ引いたものです。
+				 * @param defaultValue もし取れなかった場合は返り値になります
+				 * @returns 低いところにある物体の底辺の座標。
+				 */
+				const getLowestPos = (defaultValue: number = 0): number => {
+					let pos = defaultValue
+					const tmpList: b2.Box2DOptions.EBody[] = []
+					// 画面外の物体はいらない
+					bodyObjectList.forEach(obj => {
+						if (obj.entity.y <= g.game.height) {
+							tmpList.push(obj)
+						}
+					})
+					if (tmpList.length > 0) {
+						tmpList.sort((a, b) => {
+							if (a.entity.y < b.entity.y) return 1
+							if (a.entity.y > b.entity.y) return -1
+							return 0
+						})
+						pos = tmpList[0].entity.y - tmpList[0].entity.height
+					}
+					return pos
+				}
+				// カウントダウン処理
+				const countdownInterval = scene.setInterval(() => {
+					// ゲーム終了
+					if (time <= 0) {
+						// RPGアツマール環境であればランキングを表示します
+						if (param.isAtsumaru) {
+							const boardId = 1
+							window.RPGAtsumaru.experimental.scoreboards.setRecord(boardId, g.game.vars.gameState.score).then(() => {
+								window.RPGAtsumaru.experimental.scoreboards.display(boardId)
+							})
+						}
+						scene.clearInterval(countdownInterval) // カウントダウンを止めるためにこのイベントハンドラを削除します
+					}
+					/** ラグを考えて5秒前にはゲームが終わるようにする。ゲームが60秒間遊べるように。 */
+					if (time <= 5) {
+						// クリックイベント削除。これで遊べなくなります
+						scene.pointMoveCapture.removeAll()
+					}
+					// 時間減らしていく
+					time -= 1
+					if (time - 5 >= 0) {
+						// カウントダウン処理
+						// ゲームが遊べる時は60秒。でもゲームは65秒あるので引いておく。
+						// 5秒足して置くことで読み込み遅れても5秒なら耐えられる。
+						timeLabel.text = "残り時間: " + Math.ceil(time - 5) + "秒"
+						timeLabel.invalidate()
+					}
+				}, 1000)
+
+				// ここまでゲーム内容を記述します
+			})
+			g.game.replaceScene(scene)
+		}, 5000)
 	})
-	g.game.pushScene(scene)
-
+	// タイトル画面へ画面切り替え。
+	g.game.pushScene(titleScene)
 
 }
